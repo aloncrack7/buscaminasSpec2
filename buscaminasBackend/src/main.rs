c8806cd8e739f3010691364ffcb08e9dc23f99ad
aloncrack7 <61496485+aloncrack7@users.noreply.github.com>
2024-02-12 14:25:53 +0100

    Primerra parte rest

--- buscaminasBackend/src/main.rs
+++ buscaminasBackend/src/main.rs
@@ -1,146 +1,136 @@
+use std::collections::HashMap;
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+use std::sync::{Arc, Mutex};
 use std::thread;
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use rand::seq::index;
 
-// use crate::buscaminas::Tablero;
 mod buscaminas;
 
-fn handle_client(mut stream: TcpStream) {
-    let mut terminado=false;
-
-    while !terminado {
-        let mut buffer = [0; 1];
-        match stream.read(&mut buffer) {
-            Ok(n) => {
-                if n == 0 {
-                    terminado=true;
-                }
-
-                let opcion=buffer[0] as i8;
-                match opcion {
-                    -1=>{
-                        terminado=true;
-                    }
-                    1=>{
-                        medio(&mut stream);
-                    }
-                    2=>{
-                        dificil(&mut stream);
-                    }
-                    3=>{
-                        ia();
-                    }
-                    _ =>{
-                        terminado=true;
-                    }
-                }
-            }
-            Err(e) => {
-                eprintln!("Failed to read from connection: {}", e);
-                terminado=true;
-            }
-        }
-    }
-
-    println!("Connection closed");
+struct AppState{
+    tableros: Mutex<HashMap<String, buscaminas::Tablero>>
 }
 
-fn medio(stream: &mut TcpStream){
+fn obtenerCliente(req: &HttpRequest) -> String{
+    req.connection_info().peer_addr().unwrap_or_else(|| "unknown").to_owned()
+}
+
+async fn medio(data: web::Data<Arc<AppState>>, req: HttpRequest) -> impl Responder {
     println!("Medio");
+    let cliente=obtenerCliente(&req);
+    let mut tableros = data.tableros.lock().unwrap();
 
-    let mut tablero = 
-        buscaminas::Tablero::new(16, 16, 40);
+    if !tableros.contains_key(&cliente){
+        tableros.insert(cliente.clone(), buscaminas::Tablero::new(16, 16, 40));
+    }
+    let mut tablero = tableros.get_mut(&cliente).unwrap();
 
-    jugar(stream, &mut tablero);
+    let mut tablero=buscaminas::Tablero::new(16, 16, 40);
+
+    format!("Medio, {}", cliente)
 }
 
-fn dificil(stream: &mut TcpStream){
+async fn dificil(data: web::Data<Arc<AppState>>, req: HttpRequest) -> impl Responder{
     println!("Dificil");
+    let cliente=obtenerCliente(&req);
+    let mut tableros = data.tableros.lock().unwrap();
 
-    let mut tablero = 
-        buscaminas::Tablero::new(30, 16, 99);
+    if !tableros.contains_key(&cliente){
+        tableros.insert(cliente.clone(), buscaminas::Tablero::new(16, 16, 40));
+    }
+    let mut tablero = tableros.get_mut(&cliente).unwrap();
 
-    jugar(stream, &mut tablero);
+    let mut tablero=buscaminas::Tablero::new(16, 16, 40);
+
+    format!("Dificil, {}", cliente)
 }
 
-fn ia(){
+async fn ia(data: web::Data<Arc<AppState>>, req: HttpRequest) -> impl Responder{
     println!("IA");
+    let cliente=obtenerCliente(&req);
+    let mut tableros = data.tableros.lock().unwrap();
 
-    let mut tablero = 
-        buscaminas::Tablero::new(30, 16, 99);
+    if !tableros.contains_key(&cliente){
+        tableros.insert(cliente.clone(), buscaminas::Tablero::new(16, 16, 40));
+    }
+    let mut tablero = tableros.get_mut(&cliente).unwrap();
+
+    let mut tablero=buscaminas::Tablero::new(16, 16, 40);
+
+    format!("IA, {}", cliente)
 }
+ 
+async fn jugar(data: web::Data<Arc<AppState>>, req: HttpRequest) -> impl Responder {
+    println!("Jugar");
 
-fn jugar(stream: &mut TcpStream, tablero: &mut buscaminas::Tablero){
-    let mut terminado=false;
+    let cliente = obtenerCliente(&req);
+    let mut tableros = data.tableros.lock().unwrap();
 
-    while !terminado{
-        let mut buffer = [0; 3];
+    if !tableros.contains_key(&cliente) {
+        return HttpResponse::PreconditionFailed().body("Error: El cliente no tiene ningun tablero inicilizado");
+    }
+    let mut tablero = tableros.get_mut(&cliente).unwrap();
 
-        match stream.read(&mut buffer) {
-            Ok(n) => {
-                if n == 0 {
-                    terminado=true;
-                }
+    if let Some(opcion) = web::Query::<HashMap<String, String>>::from_query(req.query_string())
+        .ok()
+        .map(|query| query.get("opcion").and_then(|c| c.parse::<i32>().ok())) {
+        match opcion {
+            Some(-1) => {
+                // salir
+                let response = "Saliendo...";
+                return HttpResponse::Ok().body(response);
+            }
+            Some(0) => {
+                if let (Some(fila), Some(columna)) = (
+                    web::Query::<HashMap<String, String>>::from_query(req.query_string())
+                        .ok()
+                        .map(|query| query.get("fila").and_then(|f| f.parse::<usize>().ok())),
+                    web::Query::<HashMap<String, String>>::from_query(req.query_string())
+                        .ok()
+                        .map(|query| query.get("columna").and_then(|c| c.parse::<usize>().ok())),
+                ) {
+                    let casillas_descubiertas = tablero.descubrir_casilla(fila.unwrap(), columna.unwrap());
 
-                let opcion = buffer[0] as i8;
+                    let response = format!(
+                        "Descubriendo... Casillas descubiertas: {:?}",
+                        casillas_descubiertas
+                    );
 
-                if opcion==-1{
-                    terminado=true;
-                }else{
-                    let fila = buffer[1] as u8 as usize;
-                    let columna = buffer[2] as u8 as usize;
-
-                    if opcion==0{
-                        let casillasDescubiertas=tablero.descubrir_casilla(fila, columna);
-
-                        let numEnvios=[casillasDescubiertas.len() as u8];
-                        stream.write(&numEnvios);
-
-                        for infoCasilla in casillasDescubiertas{
-                            let fila:u8;
-                            let columna:u8;
-                            let valor:i8;
-                            (fila, columna, valor)=infoCasilla;
-                            stream.write(&[fila]);
-                            stream.write(&[columna]);
-                            let valor: u8 = if valor>=0 {valor as u8} else {255};
-                            stream.write(&[valor]);
-                        }
-
-                        #[cfg(debug_assertions)]
-                        println!("{}", tablero)
-                    }else{
-                        //TODO poner bandera
-                    }
+                    return HttpResponse::Ok().body(response);
+                } else {
+                    return HttpResponse::PreconditionFailed().body("Error: Missing 'fila' or 'columna' parameter.");
                 }
             }
-            Err(e) => {
-                eprintln!("Failed to read from connection: {}", e);
-                terminado=true;
+            Some(1) => {
+                // poner bandera
+                let response = "Poniendo bandera...";
+                return HttpResponse::Ok().body(response);
             }
-            
+            _ => {
+                return HttpResponse::PreconditionFailed().body("Error: Invalid option.");
+            }
         }
-
-        terminado=terminado || tablero.get_estado()==buscaminas::EstadoPartida::Ganada || 
-            tablero.get_estado()==buscaminas::EstadoPartida::Perdida;
+    } else {
+        return HttpResponse::PreconditionFailed().body("Error: Invalid option.");
     }
 }
 
-fn main() {
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
     println!("Hello, buscaminas!");
 
-    let listener=TcpListener::bind("127.0.0.1:7070").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("Connection established!");
-                thread::spawn(|| {
-                    handle_client(stream);
-                });
-            }
-            Err(e) => {
-                eprintln!("Connection failed: {}", e);
-            }
-        }
-    }
+    let app_state = Arc::new(AppState{
+        tableros: Mutex::new(HashMap::new())
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(app_state.clone()))
+            .service(web::resource("/medio").to(medio))
+            .service(web::resource("/dificil").to(dificil))
+            .service(web::resource("/ia").to(ia))
+            .service(web::resource("/jugar").to(jugar))
+    }).bind(("127.0.0.1", 7070))?.run().await
 }
